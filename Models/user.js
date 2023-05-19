2ee8de1b8db979dca70737e55837de2c85f05792
SATHIYAMOORTHY007 <sakthisampth@gmail.com>
2023-05-19 19:09:45 +0530

    update

---
+++ Models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+)
+
+module.exports = mongoose.model('User', UserSchema)
