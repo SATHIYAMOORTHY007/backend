c5c83fd5ae1ec8de9afcb42707cbf9d7dd8e193f
SATHIYAMOORTHY007 <sakthisampth@gmail.com>
2023-05-31 20:30:59 +0530

    update

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.access_token
-
+  console.log(token)
   if (!token) {
     return next(next(res.status(401)))
   }
